93f36ea7a7568c72ed206edc2cb72e82f1dd0365
cenderin <ml.alexander87@gmail.com>
2014-03-19 11:18:57 -0700

    changed profile controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,19 +1,3 @@
 class MembersController < ApplicationController
-  def index
-  end
 
-  def show
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
-  def create
-  end
 end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,18 +2,32 @@ class ProfileController < ApplicationController
   def index
   end
 
-  def show
+  def new
+    @profile = Profile.new
+  end
+
+  def create 
+    @profile = Profile.new(profile_params)
   end
 
   def update
+     @profile = Profile.find(params[:id])
+       if @profile.update_attributes(profile_params)
+        flash[:success] = "changes have been made"
+        redirect_to chef_path(@profile.id)
+       else
+        flash[:error] = "you are a failure and no one loves you"
+        render :edit
+       end
   end
 
   def edit
+    @chef = Chef.find(params[:id])
   end
 
-  def destroy
-  end
+  private
 
-  def create
+  def profile_params
+      params.require(:profile).permit(:name, :show, :speciality, :avatar)
   end
 end
