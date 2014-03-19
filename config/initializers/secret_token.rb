83f42e08128e94a0807beac7e06fce93c29a6607
DPL-4 <DPL-4@DPL-4s-iMac.local>
2014-03-19 09:14:10 -0700

    ran rails new ..

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtEventz::Application.config.secret_key_base = '4c6e64adbf47c646009499e395c1f35db35152306356e9863779cd9df9343e6df57537bf56f931f48fa0122c3402ae04ec672a50fd56c8bf6174c5acb044c0d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArtEventz::Application.config.session_store :cookie_store, key: '_art_eventz_session'
