2f817f24c704eeeb940054e40d63716a70a0af62
cenderin <ml.alexander87@gmail.com>
2014-03-24 10:50:49 -0700

    chagned layouts abit

--- app/controllers/dashboard/profiles_controller.rb
+++ app/controllers/dashboard/profiles_controller.rb
@@ -1,22 +1,27 @@
 class Dashboard::ProfilesController < Dashboard::DashboardController
 
   def index
-
   end
 
   def show
-    @profiles = Profile.find(params[:id])
+    @profile = Profile.find(params[:id])
   end
 
   def new
     @profile = Profile.new
   end
 
-
   def create 
     @profile = Profile.new(profile_params)
+    if @profile.save
+        flash[:success] = "changes have been made"
+        redirect_to dashboard_profile_path(@profile.id)
+    else
+        flash[:error] = "you are a failure and no one loves you"
+        render root_path
+    end
   end
-
+ 
   def update
      @profile = Profile.find(params[:id])
        if @profile.update_attributes(profile_params)
@@ -29,13 +34,17 @@ class Dashboard::ProfilesController < Dashboard::DashboardController
   end
 
   def edit
-    @profile = Profile.find(params[:id])
+    @member = Member.find(:id)
+    @profile = Profile.find(params[@member.id])
+   
   end
+
 
   private
 
   def profile_params
       params.require(:profile).permit(:name, :avatar, :tagline, :website1, :website2, :website3, :facebook, :twitter, :bio, :pic1, :pic2, :pic3, :pic4, :pic5, :pic6)
   end
+
 end
 
