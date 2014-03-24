5b47bb6e0b859e0e0c6dcfc12ea6ba6190c78a0f
cenderin <ml.alexander87@gmail.com>
2014-03-24 11:11:02 -0700

    added events relation to member id that we forgot

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319174217) do
+ActiveRecord::Schema.define(version: 20140324180926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140319174217) do
     t.string   "contact_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "member_id"
   end
 
   create_table "members", force: true do |t|
