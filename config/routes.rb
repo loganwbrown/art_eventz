b2563f727055d1e9f8de11723a959b3b8fc10036
cenderin <ml.alexander87@gmail.com>
2014-03-25 17:48:17 -0700

    changed layouts and added some routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ ArtEventz::Application.routes.draw do
   resources :profiles
   devise_for :members, controllers: { registrations: "registrations"}
   root 'static_pages#index'
-
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
 	namespace :dashboard do
 	  resources :events
 	  resources :profiles
