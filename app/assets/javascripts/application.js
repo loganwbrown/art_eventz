74047973215e01a3a2362d4e085d534dfb0fe0b4
cenderin <ml.alexander87@gmail.com>
2014-03-28 09:51:11 -0700

    moved some JS files around

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,28 +3,8 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require fullcalendar
-//= require nivo-lightbox.min
-//= require nivo-lightbox.js
 //= require_tree .
 $(function() {
     $("#event_date").datepicker();
     $('#calendar').fullCalendar();
-    $('.slider a').nivoLightbox({
-        effect: 'fade', // The effect to use when showing the lightbox
-        theme: 'default', // The lightbox theme to use
-        keyboardNav: true, // Enable/Disable keyboard navigation (left/right/escape)
-        clickOverlayToClose: true, // If false clicking the "close" button will be the only way to close the lightbox
-        onInit: function() {}, // Callback when lightbox has loaded
-        beforeShowLightbox: function() {}, // Callback before the lightbox is shown
-        afterShowLightbox: function(lightbox) {}, // Callback after the lightbox is shown
-        beforeHideLightbox: function() {}, // Callback before the lightbox is hidden
-        afterHideLightbox: function() {}, // Callback after the lightbox is hidden
-        onPrev: function(element) {}, // Callback when the lightbox gallery goes to previous item
-        onNext: function(element) {}, // Callback when the lightbox gallery goes to next item
-        errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
-    });
-
-
-
-
 });
