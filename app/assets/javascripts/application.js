b8974800e7afdc7cb78bd3dfa1f2102880ad9096
cenderin <ml.alexander87@gmail.com>
2014-04-01 13:56:53 -0700

    formatted html, css, and js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,11 +3,10 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require fullcalendar
-//= require jquery_nested_form
+//= require jquery.share
+//= require jquery-nivo-slider
 //= require_tree .
 // jQuery('input[type="date"]').live('click', function(e) {e.preventDefault();}).datepicker();
-
-
 $(function() {
     $("#event_date").datepicker();
     $('#calendar').fullCalendar();
@@ -18,11 +17,11 @@ $(function() {
             boxCols: 8, // For box animations
             boxRows: 4, // For box animations
             animSpeed: 500, // Slide transition speed
-            pauseTime: 4000 // How long each slide will show
-
-
+            pauseTime: 4000 
         });
-
-
+    });
+ $('#mydiv').share({
+        networks: ['pinterest', 'tumblr', 'googleplus', 'digg', 'in1', 'facebook', 'twitter', 'linkedin', 'stumbleupon'],
+        theme: 'square'
     });
 });
