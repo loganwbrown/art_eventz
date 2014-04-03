d7110c3d79ae54faebd077c0f10a5a0d450b1ab7
cenderin <ml.alexander87@gmail.com>
2014-04-03 10:43:24 -0700

    changed carrierwave, changed views abit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY_ID"],       
     :region                 => 'us-east-1'                  
   }
-  config.fog_directory  = 'dave_one'                    
+  config.fog_directory  = 'arteventz'                    
   config.fog_public     = true                                   
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  
 end
-
