fe3a3ed0534792a4ac484e16084067fd59998496
Logan Brown <logan.w.brown@gmail.com>
2014-04-09 10:44:09 -0700

    edited view pages

--- app/models/member.rb
+++ app/models/member.rb
@@ -10,10 +10,17 @@ class Member < ActiveRecord::Base
   
   after_create :email_new_member
 
+  # something here
+                :email_newsletter
+
   private
 
   def email_new_member
     MemberMailer.welcome(self).deliver
+  end
+
+  def email_newsletter
+    MemberMailer.newsletter(self).deliver
   end
 
 end
